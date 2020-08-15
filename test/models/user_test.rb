b1bb1afda898f6f535cfb78eb957892c96e0b00f
Daniel Buttig <daniel.buttig@gmail.com>
2020-08-14 21:51:53 -0500

    [NF-1] Introduces FactoryBot and DbCleaner
    This adds FactoryBot and DB Cleaner to the project. The User model tests
    are written using these newly added gems.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
+include FactoryBot::Syntax::Methods
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe User < ActiveSupport::TestCase do
+  describe "validations" do
+    it "creates a valid user" do
+      create(:user)
+      expect(User.count).must_equal 1
+    end
+
+    it "must have a password" do
+      assert_raises ActiveRecord::RecordInvalid do
+        user = create(:user, password: nil)
+      end
+    end
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,34 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/autorun'
+require 'factory_bot'
+require 'factory_bot_rails'
+require 'database_cleaner'
+require 'database_cleaner-active_record'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   #fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include FactoryBot::Syntax::Methods
+end
+
+DatabaseCleaner.strategy = :transaction
+
+module AroundEachTest
+  def before_setup
+    super
+    DatabaseCleaner.start
+  end
+
+  def after_teardown
+    super
+    DatabaseCleaner.clean
+  end
+end
+
+class Minitest::Test
+  include AroundEachTest
 end
